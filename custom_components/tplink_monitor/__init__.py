f2ffb6dee913fbf3cfa5f97da4758b03fcddd7fd
Bernhard Hecker <bernhard.hecker@gmail.com>
2025-06-27 14:49:33 +0200

    fixed config entry setup - removed deprecated function call

--- custom_components/tplink_monitor/__init__.py
+++ custom_components/tplink_monitor/__init__.py
@@ -5,20 +5,17 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up TP-Link Monitor from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-
-    # Store configuration in hass.data
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    # Reload if needed
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Remove the TP-Link integration."""
-    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
     if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
+        hass.data[DOMAIN].pop(entry.entry_id, None)
     return unload_ok
